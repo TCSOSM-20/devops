9cf4156e6cf87f4eaa96180347badcbbb4a80644
Mike Marchetti <mmarchetti@sandvine.com>
2018-10-01 12:37:59 -0400

    Update policy module dependency checking
    - dependency checking needs to be full package name
    - POL uses different naming scheme for package
    Signed-off-by: Mike Marchetti <mmarchetti@sandvine.com>
    
    Change-Id: I87c8c6c4291803562225e45b8af86df358dabfb6

--- docker/mk/get_version.sh
+++ docker/mk/get_version.sh
@@ -6,7 +6,7 @@ REPOSITORY="testing"
 REPOSITORY_BASE="http://osm-download.etsi.org/repository/osm/debian"
 DEBUG=
 
-while getopts ":r:k:u:R:b:-:dm:" o; do
+while getopts ":r:k:u:R:b:-:dm:p:" o; do
     case "${o}" in
         r)
             REPOSITORY=${OPTARG}
@@ -23,6 +23,9 @@ while getopts ":r:k:u:R:b:-:dm:" o; do
         d)
             DEBUG=y
             ;;
+        p)
+            PACKAGE_NAME=${OPTARG}
+            ;;
         m)
             MDG=${OPTARG}
             ;;
@@ -35,12 +38,14 @@ if [ -z "$MDG" ]; then
     echo "missing MDG"
 fi
 
+[ -z "$PACKAGE_NAME" ] && PACKAGE_NAME=$MDG
+
 if [ -n "$DEBUG" ]; then
     echo curl $REPOSITORY_BASE/$RELEASE/dists/$REPOSITORY/$MDG/binary-amd64/Packages
 fi
 
-curl $REPOSITORY_BASE/$RELEASE/dists/$REPOSITORY/$MDG/binary-amd64/Packages 2>/dev/null | awk -v mdg=$MDG '{
-    if ( /Package:/ && match($2,tolower(mdg)) ) {
+curl $REPOSITORY_BASE/$RELEASE/dists/$REPOSITORY/$MDG/binary-amd64/Packages 2>/dev/null | awk -v pkg=$PACKAGE_NAME '{
+    if ( /Package:/ && match($2,sprintf("%s$",tolower(pkg)) ) ) {
         package=1;
     } else if (package==1 && match($1,"Version:")) { 
         package=0; 
