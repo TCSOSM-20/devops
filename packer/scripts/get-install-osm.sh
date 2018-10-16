fdf74c5e681db879856b27230176eeef9637f46b
Mike Marchetti <mmarchetti@sandvine.com>
2018-10-16 10:36:51 -0400

    Update osm build to use v4.0 scripts
    Change-Id: I5ba21d1c07cc4399a13a9ef30336250a9c76948b
    Signed-off-by: Mike Marchetti <mmarchetti@sandvine.com>

--- packer/scripts/get-install-osm.sh
+++ packer/scripts/get-install-osm.sh
@@ -3,9 +3,9 @@ export PATH=$PATH:/snap/bin
 echo "PATH=$PATH"
 juju status
 
-wget https://osm-download.etsi.org/ftp/osm-5.0-five/install_osm.sh
+wget https://osm-download.etsi.org/ftp/osm-4.0-four/install_osm.sh
 chmod +x install_osm.sh
-./install_osm.sh --nolxd --nodocker --nojuju -y
+./install_osm.sh --nolxd --nodocker -y
 
 cat >> ~/.bashrc <<-EOF
 export OSM_HOSTNAME=127.0.0.1
