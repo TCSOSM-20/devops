0b41d73d925df237fea2a437b200a48689c498bb
Mike Marchetti <mmarchetti@sandvine.com>
2018-10-16 11:49:29 -0400

    install docker as part of osm
    Change-Id: I094d6741c3527c30ee628d1ff19fc5454e43d0fc
    Signed-off-by: Mike Marchetti <mmarchetti@sandvine.com>

--- packer/scripts/get-install-osm.sh
+++ packer/scripts/get-install-osm.sh
@@ -5,7 +5,7 @@ juju status
 
 wget https://osm-download.etsi.org/ftp/osm-4.0-four/install_osm.sh
 chmod +x install_osm.sh
-./install_osm.sh --nolxd --nodocker -y
+./install_osm.sh --nolxd -y
 
 cat >> ~/.bashrc <<-EOF
 export OSM_HOSTNAME=127.0.0.1
