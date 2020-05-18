7ca1ec63172ee3e98ce9ad56bc925b90ef771b49
David Garcia <david.garcia@canonical.com>
2020-05-18 18:20:43 +0200

    Change bootstrapping series back to xenial.
    This is a temporary fix. Jenkins slave can't bootstrap a bionic series right now.
    
    Change-Id: I28da224c093a102a964f15bcbd7fadfca771b9af
    Signed-off-by: David Garcia <david.garcia@canonical.com>

--- installers/full_install_osm.sh
+++ installers/full_install_osm.sh
@@ -395,7 +395,7 @@ function juju_createcontroller() {
     if ! juju show-controller $OSM_STACK_NAME &> /dev/null; then
         # Not found created, create the controller
         sudo usermod -a -G lxd ${USER}
-    sg lxd -c "juju bootstrap $OSM_VCA_CLOUDNAME $OSM_STACK_NAME"
+        sg lxd -c "juju bootstrap --bootstrap-series=xenial $OSM_VCA_CLOUDNAME $OSM_STACK_NAME"
     fi
     [ $(juju controllers | awk "/^${OSM_STACK_NAME}[\*| ]/{print $1}"|wc -l) -eq 1 ] || FATAL "Juju installation failed"
 }
